29f717c41b97441fbe2468a9ba770e027b77c51a
Crowdbotics <team@crowdbotics.com>
2024-01-19 00:43:32 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "stealthfinapp-45441",
+  url: "https://stealthfinapp-45441.botics.co",
+  api: "https://stealthfinapp-45441.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
